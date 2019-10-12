e811d995c60dddc93b7b38a7c59a339f42cc4786
Talasan Nicholson <talasan.nicholson@gmail.com>
2019-10-12 13:27:24 -0500

    quick gal commit

--- pages/api/calendar.ts
+++ pages/api/calendar.ts
@@ -58,21 +58,43 @@ export default (_: NextApiRequest, res: NextApiResponse) => {
     })
 }
 
-const render = (res: NextApiResponse) => (data: Item[] = []) =>
-  res.send(
+const render = (res: NextApiResponse) => (data: Item[] = []) => {
+  res.writeHead(200, {
+    'Content-Type': 'text/calendar; charset=utf-8',
+    'Content-Disposition': 'attachment; filename="calendar.ics"'
+  })
+
+  res.end(
     ical({
       domain: 'talasan.dev',
       name: 'FX Calendar',
       prodId: { company: 'talasan', product: 'fx-cal' },
-      events: data.map(d => ({
-        start:
-          d.time && !/all/i.test(d.time)
-            ? moment(`${d.date} ${d.time.replace(/(pm|am)/i, ' $1')}`)
-            : moment(d.date),
-        summary: d.title
-      }))
+      events: data
+        .filter(d => d.title)
+        .map(({ time, date, title: summary }) => {
+          try {
+            const start =
+              time && !/all/i.test(time)
+                ? moment(`${date} ${time.replace(/(pm|am)/i, ' $1')}`)
+                : moment(date)
+
+            const end = moment(start).add(15, 'minutes')
+
+            return {
+              description: summary,
+              end,
+              organizar: 'FX',
+              start,
+              summary
+            }
+          } catch (e) {
+            return null
+          }
+        })
+        .filter(v => v)
     }).toString()
   )
+}
 
 interface Item {
   date: string
