5fd0811a542c061b3aadf838e9d4e648bcbb7004
Talasan Nicholson <talasan.nicholson@gmail.com>
2019-10-12 13:04:53 -0500

    quick gal commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 module.exports = withPlugins(
   [withCSS, [offline, ['!', PHASE_DEVELOPMENT_SERVER]]],
   {
-    target: 'serverless',
+    target: 'server',
     webpack(config) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|ico)$/i,
