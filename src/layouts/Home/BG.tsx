a39641e27e788e41e325d0ca01c9e1c1d93d01ce
Talasan Nicholson <talasan.nicholson@gmail.com>
2022-03-04 18:15:50 -0600

    chore: uptick

--- src/layouts/Home/BG.tsx
+++ src/layouts/Home/BG.tsx
@@ -1,30 +1,25 @@
+import { useAspect } from '@react-three/drei'
 import type { Props as CanvasProps } from '@react-three/fiber'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Suspense, useRef } from 'react'
 import { Mesh } from 'three'
 
 const Scene = () => {
+  const scale = useAspect(1920, 1600)
   const ref = useRef<Mesh>()
 
   useFrame(() => {
     if (ref.current instanceof Mesh) {
-      ref.current.rotation.x += 0.001
-      ref.current.rotation.z -= 0.001
+      ref.current.rotation.x += 0.0005
+      ref.current.rotation.z -= 0.0005
     }
   })
 
   return (
-    <>
-      <mesh {...{ ref }}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial
-          color="#ff72ba"
-          opacity={0.15}
-          transparent
-          wireframe
-        />
-      </mesh>
-    </>
+    <mesh {...{ ref, scale }}>
+      <sphereGeometry args={[2 / 3, 50 / 3, 1]} />
+      <meshBasicMaterial color="#ff72ba" opacity={0.15} transparent wireframe />
+    </mesh>
   )
 }
 
@@ -32,7 +27,7 @@ export default function BG(props: Partial<CanvasProps>) {
   return (
     <Canvas
       camera={{ position: [0, 0, 1] }}
-      dpr={[2, 2]}
+      dpr={[2, 3]}
       flat
       mode="concurrent"
       {...props}>
