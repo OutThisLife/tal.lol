3358b67019077d2155e23c54e2c8c14f0f02ce30
Brooklyn <brooklyn.bb.nicholson@gmail.com>
2024-07-30 14:16:06 -0500

    chore: uptick

--- src/layouts/Home/BG.tsx
+++ src/layouts/Home/BG.tsx
@@ -1,12 +1,11 @@
-import { useAspect } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
 import type { Props as CanvasProps } from '@react-three/fiber'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Suspense, useRef } from 'react'
 import { Mesh } from 'three'
 
 const Scene = () => {
-  const scale = useAspect(1920, 1600)
-  const ref = useRef<Mesh>()
+  const ref = useRef<Mesh>(null!)
 
   useFrame(() => {
     if (ref.current instanceof Mesh) {
@@ -16,26 +15,75 @@ const Scene = () => {
   })
 
   return (
-    <mesh {...{ ref, scale }}>
-      <sphereGeometry args={[0.5, 85, 1]} />
-      <meshBasicMaterial color="#ff72ba" opacity={0.15} transparent wireframe />
+    <mesh {...{ ref }}>
+      <octahedronGeometry args={[1, 15]} />
+      <meshStandardMaterial
+        color="#ff72ba"
+        opacity={0.1}
+        transparent
+        wireframe
+        onBeforeCompile={v => {
+          v.vertexShader = v.vertexShader
+            .replace(
+              '#include <common>',
+              `
+      #include <common>
+      varying vec3 vTransformedNormal;
+      varying vec3 vPosition;
+      `
+            )
+            .replace(
+              '#include <beginnormal_vertex>',
+              `
+      #include <beginnormal_vertex>
+      vPosition = position;
+      vTransformedNormal = normalize(normalMatrix * objectNormal);
+      `
+            )
+
+          v.fragmentShader = v.fragmentShader
+            .replace(
+              '#include <clipping_planes_pars_fragment>',
+              `
+      #include <clipping_planes_pars_fragment>
+      
+      uniform float bottomY;
+      uniform vec3 color;
+
+      varying vec3 vTransformedNormal;
+      varying vec3 vPosition;
+      `
+            )
+            .replace(
+              '#include <dithering_fragment>',
+              `
+      #include <dithering_fragment>
+      vec3 viewDirection = normalize(-vViewPosition);
+
+      if (
+        (bottomY > 0. && vPosition.y <= -bottomY) 
+        || dot(vTransformedNormal, viewDirection) <= 0.0
+      ) {
+        discard;
+      }
+
+      if (color.r > 0. || color.g > 0. || color.b > 0.) {
+        gl_FragColor.rgb = color;
+      }
+      `
+            )
+        }}
+      />
     </mesh>
   )
 }
 
 export default function BG(props: Partial<CanvasProps>) {
   return (
-    <Canvas
-      camera={{ position: [0, 0, 1] }}
-      dpr={[2, 3]}
-      flat
-      mode="concurrent"
-      {...props}>
-      <ambientLight />
-      <spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
-      <pointLight position={[-10, -10, -10]} />
+    <Canvas camera={{ position: [0, 0, 0], zoom: 1e3 }} orthographic {...props}>
+      <Environment preset="city" />
 
-      <Suspense fallback={null}>
+      <Suspense>
         <Scene />
       </Suspense>
     </Canvas>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,23 @@ import { Home } from '~/layouts'
 
 export default function Index() {
   return (
-    <Suspense fallback={null}>
+    <Suspense>
       <Home
         bio={`I am a full stack engineer and have been coding since I was ~12.\nMy primary focus is to create beautiful, stunning experiences with the best designers available.\nIn tandem, I push to keep perf. bottlenecks and cost near ~0.\n`}
         experience={[
-          { company: 'outthislife', date: 'now', position: '*' },
+          { company: 'brookie', date: 'now', position: '*' },
+          {
+            company: 'ExtendAI',
+            date: '2024',
+            position: 'sr full stack',
+            url: '//www.extend.ai'
+          },
+          {
+            company: 'Braintrust',
+            date: '2024',
+            position: 'sr full stack',
+            url: '//www.braintrust.com'
+          },
           {
             company: 'ClubNFT',
             date: '2022-2023',
@@ -40,10 +52,10 @@ export default function Index() {
           {
             company: 'GKW',
             date: '2010-2017',
-            position: 'Lead',
+            position: 'dev',
             url: '//www.thegkwco.com'
           },
-          { company: 'Get Aha', date: '2010-2014', position: 'Lead' },
+          { company: 'Get Aha', date: '2010-2014', position: 'dev' },
           {
             company: 'Lucid Crew',
             date: '2013',
@@ -92,7 +104,7 @@ export default function Index() {
           'GraphQL, SQL, NoSQL, MongoDB',
           'Ps, Ai, Sketch, Cinima 4D, Maya'
         ]}
-        title="Brooklyn Nicholson"
+        title="Brooklyn! Nicholson"
       />
     </Suspense>
   )
